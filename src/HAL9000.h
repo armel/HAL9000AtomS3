2c4751a2389058c77944bd3319b89a1d388bbc3d
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2023-07-23 19:18:37 +0200

    Add brightness control

--- src/HAL9000.h
+++ src/HAL9000.h
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 // Version
-#define VERSION "1.0.0"
+#define VERSION "1.1.0"
 #define AUTHOR  "F4HWN"
 #define NAME    "HAL9000"
 
@@ -51,8 +51,9 @@ uint8_t brightness   = 32;
 uint8_t limit        = 10;
 
 #if BOARD == ATOMS3
-//#define GFX_DEV_DEVICE ARDUINO_M5Stack_ATOMS3
+// #define GFX_DEV_DEVICE ARDUINO_M5Stack_ATOMS3
 #define GFX_BL 16
-Arduino_DataBus *bus = new Arduino_ESP32SPI(33 /* DC */, 15 /* CS */, 17 /* SCK */, 21 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
+Arduino_DataBus *bus =
+  new Arduino_ESP32SPI(33 /* DC */, 15 /* CS */, 17 /* SCK */, 21 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
 Arduino_GFX *gfx = new Arduino_GC9107(bus, 34 /* RST */, 0 /* rotation */, true /* IPS */);
 #endif
